9013da31a323b8a92c01612d64bb9ba4cdeefe8c
briannebyer <briannemareebyer@mail.com>
2023-04-26 10:13:51 +1000

    Initial Commit

---
+++ FavouritePlaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavouritePlaces
+//
+//  Created by Brianne Byer on 26/4/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FavouritePlaces/FavouritePlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouritePlacesApp.swift
+//  FavouritePlaces
+//
+//  Created by Brianne Byer on 26/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouritePlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
